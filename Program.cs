7d388c8dbec38679914c8bf85844406f8af03670
Lacey Walker <bluelarksong@gmail.com>
2020-11-16 18:51:21 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassBasics
+{
+    // class Program
+    // {
+    //     static void Main(string[] args)
+    //     {
+    //         Console.WriteLine("Hello World!");
+    //     }
+    // }
+
+    public class Writer
+    {
+        public Writer(string prefix)
+        {
+            Prefix = prefix;
+        }
+
+        public string Prefix { get; set; }
+
+        public void Write(string message)
+        {
+            Console.WriteLine(Prefix + message);
+
+            // The output variable's type is `string` -- a built in type
+            string output = "Nashville Software School";
+
+            // The author variable's type is Writer -- a custom type you defined
+            Writer author = new Writer("I do declare: ");
+            author.Write(output);
+
+            author.Prefix = "That's right, I said: ";
+            author.Write(output);
+        }
+
+    }
+}
